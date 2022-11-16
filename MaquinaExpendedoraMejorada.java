05a0d843a05b509e81451bf5253697e15dcd617d
Nicolas <nicoaldopaez@gmail.com>
2022-11-16 10:52:46 +0100

    cantidaddedineroquefalta2

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -54,8 +54,8 @@ public class MaquinaExpendedoraMejorada {
      * Imprime un billete para el cliente actual
      */
     public void imprimirBillete() {
-        int cantidadDeDineroQueFalta=(precioBillete-balanceClienteActual);
-        if (balanceClienteActual >= precioBillete) {        
+        int cantidadDeDineroQueFalta = (precioBillete - balanceClienteActual);
+        if (cantidadDeDineroQueFalta <= 0) {        
             // Simula la impresion de un billete
             System.out.println("##################");
             System.out.println("# Billete de tren:");
